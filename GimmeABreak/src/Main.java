0ee323593a65e59b084372c2b3c7684be5775718
Faris MA <92819860+Fabsheru77@users.noreply.github.com>
2025-05-01 11:25:12 -0700

    Add files via upload

---
+++ GimmeABreak/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 7;
+		int total = add(a,b);
+		System.out.printf("%d + %d = %d",a,b,total);
+
+	}//end main()
+	
+	private static int add(int a, int b) {
+		return a + b;
+	}//end add()
+
+}//end Main class
